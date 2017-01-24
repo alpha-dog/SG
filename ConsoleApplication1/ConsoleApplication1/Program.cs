37cf5dd2ba43c6f97c456a29c644b85963b66af1
evans363 <david.ballenger.evans@gmail.com>
2017-01-24 13:22:17 -0600

    DAEVINDWRK-1 trying to figure out jira stuff

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep(); //why does beep only work when placed before WriteLine and ReadLine
+            Console.WriteLine("hello, worldd");
+            Console.ReadLine();
+            //types describe what sort of data you're talking about   
+        }
+    }
+}
