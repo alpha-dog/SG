1ab287bcf71c1b947a4aea7785b010a3a18e8697
evans363 <david.ballenger.evans@gmail.com>
2017-01-26 22:58:35 -0600

    updated guessingGame. its done except for the 'press q to quit' specification

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -11,60 +11,95 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             int theAnswer;
-            int playerGuess;
+            int playerGuess = 0;
             string playerInput;
+            int guessCounter = 0;
+            int mode = 20; 
 
-            Random r = new Random();
-            theAnswer = r.Next(1, 21);
-
+            //ask for name
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("what's your name?");
             string bro = Console.ReadLine();
-            //ReadKey for number 3? 
+
+            //ask if they want easy, medium, or hard mode
+            Console.WriteLine("Do want easy, medium, or hard mode");
+            string hardness = Console.ReadLine();
+
+            //assign hardness to 'mode' variable
+            if (hardness == "easy")
+            {
+                mode = 5;
+            }
+            else if (hardness == "hard")
+            {
+                mode = 50;
+            }
+            else
+            {
+                mode = 20; // mode defaults to 20 guesses (medium hardness) because that was the original spec and people are most likely to mis-spell 'medium'
+            }
+
+            //generate radom number
+            Random r = new Random();
+            theAnswer = r.Next(1, mode); //was this a 21 in the original
+
+
             // get player input
-            Console.Write(bro + ", Enter your guess (1-20): ");
+            Console.Write(bro + ", Enter your guess (1- " + mode + "): ");
 
             do
             {
-
-
                 //attempt to convert the string to a number
                 while (true)
                 {
-
+                     
                     playerInput = Console.ReadLine();
                     int.TryParse(playerInput, out playerGuess);
 
-                    if (playerGuess >= 1 && playerGuess <= 20)
+                    if (playerGuess >= 1 && playerGuess <= mode)
                     {
+                        guessCounter++;
+                        Console.WriteLine("guess# " + guessCounter);
                         break;
                     }
                     else
                     {
-                        Console.WriteLine("wrong. enter a number between 1 and 20");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(bro +" wrong. enter a number between 1 and " + mode);
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
                 }
-
                 if (playerGuess == theAnswer)
                 {
-                    Console.WriteLine($"{theAnswer} was the number.  You Win!");
-                    Console.ReadLine();
-                    break;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    if (guessCounter == 1)
+                    {
+                        Console.WriteLine(bro + " !!!!!!! WOW YOU WON ON THE FIRST TRY !!!!!!!");
+                        Console.ReadLine();
+                        Console.ResetColor();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{theAnswer} was the number.  You Win! " + bro);
+                        Console.ReadLine();
+                        break;
+                    }             
                 }
                 else
                 {
                     if (playerGuess > theAnswer)
                     {
-                        Console.WriteLine("Your guess was too High!");
+                        Console.WriteLine(bro + " Your guess was too High!");
                     }
                     else
                     {
-                        Console.WriteLine("Your guess was too low!");
+                        Console.WriteLine(bro + " Your guess was too low!");
                     }
                 }
-
 
             } while (true);
         }
     }
-}
+} 
 
