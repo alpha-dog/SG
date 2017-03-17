52dc05c04d71b12594d000bbd52af85e2a0ec830
evans363 <david.ballenger.evans@gmail.com>
2017-03-17 09:39:49 -0500

    dynamically built rows and columns for the snacks work, but the vending column on the right needs to be added

--- VendingMachine/js/home.js
+++ VendingMachine/js/home.js
@@ -7,8 +7,10 @@ $(document).ready(function () {
 });
 function loadSnacks(){
     
-    var snackStuff = $('#snack')
     
+    var columnCount = 1;
+    var newRow = true;
+    var rowCount = 1;
 
     $.ajax({
         type: 'GET',
@@ -19,19 +21,45 @@ function loadSnacks(){
                 var name = snack.name;
                 var price = snack.price;
                 var quantity = snack.quantity;
+                
+
                             //make it even more dynamic and add an id for button that uses the index to number it
                     //    var newSnack = '<div class="panel panel-default" id="snack"> <button> <div class="panel-heading">';
-                      var  newSnack = '<button> <div class="panel-heading"> <h4>' + name + '</h4>'; 
+                      var  newSnack = '';
+                      var newSnackRow = '';
+                           if(newRow)
+                           {
+                               newSnackRow += '<div class="row first-row text-center row' + rowCount + '">\
+            <div class="col-md-3 col-sm-4 col-xs-6 bottom-gutter column1"><div class="panel panel-default span4"  id="snack"></div></div>\
+            <div class="col-md-3 col-sm-4 col-xs-6 bottom-gutter column2"><div class="panel panel-default span4"  id="snack"></div></div>\
+            <div class="col-md-3 col-sm-4 col-xs-6 bottom-gutter column3"><div class="panel panel-default span4"  id="snack"></div></div></div>'
+            $('.container').append(newSnackRow);
+            
+                           }
+                           var snackStuff = $('.row' + rowCount + ' .column' + columnCount + ' #snack');
+                           newSnack += '<button> <div class="panel-heading"> <h4>' + name + '</h4>'; 
                            newSnack += '</div> <div class="panel-body pheat-body">';
                            newSnack += '<h4>' + price + '</h4>'; 
                            newSnack += '</div> <div class="panel-footer">';
                            newSnack += '<h4>' + quantity + '</h4>'; 
                            newSnack += '</div> </button> </div>';
-                
+
                 
                 //  var newSnack = '<h4>' + name + index + '<h4>';
 
                 snackStuff.append(newSnack);
+                if(columnCount >= 3)
+                {
+                    newRow = true;
+                    rowCount++;
+                    columnCount = 0;
+                }
+                else
+                {
+                    newRow = false;
+                }
+                columnCount++;
+                
             });
 
         },
