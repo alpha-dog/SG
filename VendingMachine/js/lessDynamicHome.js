f510209ec46646a76e212a69d4918fdd56c1a7fe
evans363 <david.ballenger.evans@gmail.com>
2017-03-18 01:59:05 -0500

    things to get working: change display, change getter button, error messages, Q updateer

--- VendingMachine/js/lessDynamicHome.js
+++ VendingMachine/js/lessDynamicHome.js
@@ -1,48 +1,102 @@
 //this is the javascript file for my vendig machine
 "use strict";
+
+var total = 0;
+var itemSelected;
+
 $(document).ready(function () {
 
     loadSnacks();
-
+    $('#makePurchase').click(vendSnack);
+    
 });
 
-function loadSnacks(){
+function loadSnacks() {
     var snackNum = 1;
 
     $.ajax({
         type: 'GET',
         url: 'http://localhost:8080/items',
-        success: function(contactArray) {
-            $.each(contactArray, function(index, items) {
-                
-                var contentRows = $('#snack' + snackNum)
-               
+        success: function (contactArray) {
+            $.each(contactArray, function (index, items) {
+
+                var contentRows = $('#snack' + snackNum);
+
                 var name = items.name;
                 var price = items.price;
                 var quantity = items.quantity;
-                
 
-                var snackLoader = ' <button>\
+
+                var snackLoader = ' <button value=' + snackNum + ' class="snackButton" data-id="' + snackNum + '">\
                                         <div class="panel-heading">\
-                                            '+ name +'\
+                                            '+ snackNum + ' ' + name + '\
                                         </div>\
                                         <div class="panel-body pheat-body">\
-                                            '+ price +'\
+                                            $'+ price + '\
                                         </div>\
                                         <div class="panel-footer">\
-                                            '+ quantity +'\
+                                            Quantity Left: ' + quantity + '\
                                         </div>\
                                     </button>';
-                  snackNum++;                  
-                
+                snackNum++;
+
 
                 contentRows.append(snackLoader);
             });
+            $('.snackButton').click(function () {
+                itemSelected = $(this).data("id");
+                $('#snackSelection').val(itemSelected);
+            });
+
         },
-        error: function(){
-            $('#errorMessages')
-                .append($('<li>').attr({class: 'list-group-item list-group-danger'})
-                .text('Error calling web service. Please try again later.'));
+        error: function () {
+            $('#message')
+                .append($('<li>').attr({ class: 'list-group-item list-group-danger' })
+                    .text('Error calling web service. Please try again later.'));
         }
     });
 }
+function vendSnack(){
+
+    $.ajax({
+        type: 'GET',
+        ///money/{amount}/item/{id}
+        url: 'http://localhost:8080/money/'+total+'/item/'+itemSelected+'',
+        success: function(change, status) {
+            $('#changeDisplay').text('Q:'+change.quarters + 'D:' + change.dimes + 'N:' + change.nickels + 'P:' + change.pennies);
+            $('#snackMessage').val('enjoy your snack');
+            total = 0;
+            $('#totalMoney').val(total);
+        },
+        error: function(){
+            $('#snackMessage').text(change.message); 
+        }
+    })
+}
+
+$('#addDollar').click(function () {
+    total += 1;
+    $('#totalMoney').val(total);
+});
+
+$('#addQuarter').click(function () {
+    total += .25;
+    $('#totalMoney').val(total);
+});
+
+$('#addDime').click(function () {
+    total += .10;
+    $('#totalMoney').val(total);
+});
+
+$('#addNickel').click(function () {
+    total += .05;
+    $('#totalMoney').val(total);
+});
+
+$('#snackButton1').click(function () {
+    itemSelected = 1;
+    $('#snackSelection').val(itemSelected);
+});
+
+
